1134d579d06e4f5e64e66618b11692737f04818d
Quinten Peels <quinten@peels.me>
2021-03-04 15:42:31 +0100

    404 fallback

---
+++ src/views/NotFound.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="columns">
+    <div class="column">
+      <h1 class="is-size-4 m-b-md has-text-centered">
+        404 - Page not found
+      </h1>
+    </div>
+  </div>
+</template>
